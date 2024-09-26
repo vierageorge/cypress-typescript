813d859d815c4afd92c140826000ffa6670b8d63
vierageorge <joviera@gmail.com>
2024-09-26 15:29:41 -0500

    Initial commit

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,13 @@
+// import type { MyType } from './types';
+
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       /**
+//        * Custom command to select DOM element by data-cy attribute.
+//        * @example cy.dataCy('greeting')
+//        */
+//       dataCy(value: MyType): Chainable<Element>; // This works
+//     }
+//   }
+// }
